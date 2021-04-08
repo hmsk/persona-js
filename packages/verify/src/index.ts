29549f669e4a171462a27cd28c1859f9ce57ead7
hmsk <k.hamasaki@gmail.com>
2021-04-07 22:05:17 -0700

    Support environment

--- packages/verify/src/index.ts
+++ packages/verify/src/index.ts
@@ -1,26 +1,46 @@
-const verifyClient = (templateId: string, host?: string) => {
-  const id = templateId
-  const hostName = host ?? 'withpersona.com'
-  const listeners = []
+const verifyClient = (
+  templateId: string,
+  environment: "sandbox" | "production" = "sandbox",
+  host: string = "withpersona.com"
+) => {
+  const id = templateId;
+  const listeners = [];
 
-  const on = (eventName: 'complete' | 'fail' | 'start', listener: (inquiryId: string) => void) => {
-    listeners.push(listener)
-    return self
-  }
+  const on = (
+    eventName: "complete" | "fail" | "start",
+    listener: (inquiryId: string) => void
+  ) => {
+    listeners.push(listener);
+    return self;
+  };
 
   const start = () => {
-    const iframeUrl = `https://${hostName}/verify/${templateId}`
-    alert(iframeUrl)
-  }
+    const iframeUrl = `https://${host}/widget/${templateId}&environment=${environment}`;
+    alert(iframeUrl);
+    /*
+      Build this programmatically
+      <iframe
+        allow='camera'
+        sandbox='allow-same-origin allow-scripts allow-forms allow-popups allow-modals allow-top-navigation-by-user-activation'
+        frameBorder='0'
+        src={baseUrl + '/widget' + queryParams}
+      ></iframe>
+    */
+  };
+
+  const getHostedFlowUrl = () => {
+    const hostedUrl = `https://${host}/verify?template-id=${templateId}&environment=${environment}`;
+    alert(hostedUrl);
+  };
 
   const self = {
-    getHostedFlowUrl: () => {},
+    getHostedFlowUrl,
     on,
     prefill: () => {},
     start,
-  }
+  };
 
-  return self
-}
+  return self;
+};
 
-export default verifyClient
+export default verifyClient;
