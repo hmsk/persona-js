88370d9462dbd912fb832d03d7a0667c2ba4fa70
hmsk <k.hamasaki@gmail.com>
2021-04-19 23:26:10 -0700

    Jack create-next-app design to run flows

--- examples/next/pages/index.js
+++ examples/next/pages/index.js
@@ -7,54 +7,29 @@ export default function Home(props) {
   const persona = newInquiry(props.templateId)
   return (
     <div className={styles.container}>
-      <button onClick={() => persona.start()}>Start Embedded Flow</button>
-      <button onClick={() => persona.getHostedFlowUrl()}>Get a URL for Hosted Flow</button>
       <Head>
-        <title>Create Next App</title>
+        <title>@persona-js Demo Next App</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-
-        <p className={styles.description}>
-          Get started by editing <code className={styles.code}>pages/index.js</code>
-        </p>
+        <h1 className={styles.title}>@perosona-js demo with Next.js</h1>
 
         <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
+          <a href="#" className={styles.card} onClick={() => persona.start()}>
+            <h3>Start Embedded Flow &rarr;</h3>
+            <p>Open an iframe based verification flow.</p>
           </a>
 
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a href="https://github.com/vercel/next.js/tree/master/examples" className={styles.card}>
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>Instantly deploy your Next.js site to a public URL with Vercel.</p>
+          <a href="#" className={styles.card} onClick={() => window.open(persona.getHostedFlowUrl())}>
+            <h3>Open Hosted Flow &rarr;</h3>
+            <p>Open a new tab to get through hoted verification flow.</p>
           </a>
         </div>
       </main>
 
       <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
+        <a href="https://vercel.com" target="_blank" rel="noopener noreferrer">
           Powered by <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
         </a>
       </footer>
